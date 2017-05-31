f5ee342dafb39c5d0d502702046175080bbf7e41
Renuka Gumaste <renuka.gumaste@gmail.com>
2017-05-31 19:42:35 +0200

    Dr.Search version 3
    creating an array and binding it to records from Firebase database using angular component 'controller'.

--- ACS/JS FIles/Dr.Search.js
+++ ACS/JS FIles/Dr.Search.js
@@ -1,20 +1,35 @@
 var app = angular.module("DrsearchApp",["firebase"]);
 
-app.controller("DrsearchController",function($scope, $firebaseObject)
+app.controller("DrsearchController",function($scope,$firebaseObject)
 {
     $scope.onSubmit = function()
   {
-    console.log("Console statement.");
-    //var inputCity=$scope.formModel;
-  
-    alert($scope.selectedCategory + " and " + $scope.selectedCity + "and" + $scope.selectedInsurance);
-
+    //console.log("Console statement.");
+  //  alert($scope.selectedCategory + " and " + $scope.selectedCity + "and" + $scope.selectedInsurance);
+    $scope.records = new Array();
     var cityRef = firebase.database().ref("doctors");
     cityRef.orderByChild("city").equalTo($scope.selectedCity).on("child_added", function(snapshot){
-        if(snapshot.child("specialization").val().includes($scope.selectedCategory) == true || 
-        snapshot.child("insurances").val().includes($scope.selectedInsurance) == true)
-         { alert("Result: " + snapshot.val().name +" and " + snapshot.child("insurances").val()  +" and " + snapshot.child("specialization").val()); } 
 
-    }); 
-  };   
-});
+        if(snapshot.child("specialization").val().includes($scope.selectedCategory) == true || 
+        snapshot.child("insurances").val().includes($scope.selectedInsurance) == true){
+
+        var Name = snapshot.child("name").val();
+        var Insurance= snapshot.child("insurances").val();
+        var Category = snapshot.child("specialization").val();
+        var image = ""
+
+        $scope.records.push({"Name":Name,"Insurance":Insurance,"Category":Category,"Image" : "Image cha path jithun image load honar ahe."} );                            
+       console.log($scope.records.length);
+       }
+  })
+                  
+    for (var i = 0; i < $scope.records.length; i++) {
+      //$scope.recordArraynew.push(recordArraynew[i]);
+      console.log("Name : " + i + $scope.records[i].Name);
+      console.log("Insurance : " + i + $scope.records[i].Insurance);
+      console.log("Category : " +i + $scope.records[i].Category);
+    }
+
+  }
+}); 
+
