ecd64334b2b4e69f828426dc768600df3d69dba6
Renuka Gumaste <renuka.gumaste@gmail.com>
2017-06-18 23:59:16 +0200

    drList.js
    taking values of rating and comments from ng-model and taking system date and storing all these things in variable.

--- Advanced Computer Science/scripts/drList.js
+++ Advanced Computer Science/scripts/drList.js
@@ -234,7 +234,38 @@ if(directionsDisplay != null) {
         });
 
   }
-                                                
+   
+  
+  scope.formData={}; 
+ 
+$scope.onComment=function(name,SelectedCity)
+{
+    var rate= $scope.formData.rate;
+    console.log("Ratings = " + rate);
+    console.log("main key rate: "+ name);
+    var comment= $scope.formData.alltext;
+    var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+today = mm+'-'+dd+'-'+yyyy;
+
+    if(comment==null || rate==null)
+    {
+      alert("Enter a valid comment.");
+      return;
+    }
+    console.log(" var comment :"+comment);
+}   
                                                  
  
 });
