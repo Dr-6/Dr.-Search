a763b56815c2641c350e2e16bc4719563400a8e8
Rishikesh Shinde <Rishikesh-07@users.noreply.github.com>
2017-06-18 23:38:26 +0200

    medicalList.js
    map implementation

--- Advanced Computer Science/scripts/medicalList.js
+++ Advanced Computer Science/scripts/medicalList.js
@@ -37,4 +37,68 @@ app.controller('listDetCtrl', ['$scope', '$routeParams','$rootScope','$firebaseO
   .catch(function(error) {
     console.log("Error:", error);
   });
+        /*map implementation*/	
+var map;
+var marker;
+var currentLat;
+var currentLon;
+var currentLatLon;
+var prevLati;
+var prevLongi;
+var isShowingSomething = false;
+var markerArray = [];
+var directionsDisplay;
+
+  $scope.initMap = function(){
+                  
+                  if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+                currentLat= position.coords.latitude;
+                currentLon= position.coords.longitude;
+       
+           var myCenter = new google.maps.LatLng(currentLat,currentLon);//(51.508742,-0.120850);
+                 currentLatLon = new google.maps.LatLng(currentLat, currentLon);
+                 var mapCanvas = document.getElementById("map");
+               
+                 var mapOptions = {center: myCenter, zoom: 14,  mapTypeId: google.maps.MapTypeId.ROADMAP};
+                 map = new google.maps.Map(mapCanvas, mapOptions);
+                 marker = new google.maps.Marker({
+                 position: myCenter,
+                 animation: google.maps.Animation.DROP
+                 });                
+                 marker.setMap(map);
+                 markerArray.push(marker);
+                 
+                 }
+            }
+                                                 
+ $scope.setMarker = function(lati,longi){
+    
+    console.log(lati);
+    
+    var latlng = new google.maps.LatLng(lati, longi);
+    map.setZoom(14);
+    map.setCenter(latlng);
+
+    marker = new google.maps.Marker({
+                 position: latlng,
+                 animation: google.maps.Animation.DROP
+                 });   
+
+    marker.setPosition(latlng);
+    markerArray.push(marker);
+
+        $scope.distanceCalculator(lati,longi);
+        document.getElementById('mode').addEventListener('change', function() {
+                
+          $scope.distanceCalculator(lati, longi);
+        });
+
+  } 
+                                                
+                                                 
+ 
+});
+
 }]);
+
